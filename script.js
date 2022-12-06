88b55b34185c814b1875795e26004ffc25656820
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2022-12-06 02:34:08 -0800

    added local storage function in JS

--- script.js
+++ script.js
@@ -2,19 +2,63 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  
+  // Clear all the notes when a new day starts
+  if (currentTime === 0) {
+    localStorage.clear();
+  }
 
+  // Click listeners to save the notes
+  var saveBtn = document.querySelectorAll(".saveBtn");
+  var textarea = document.getElementsByTagName("textarea");
 
+  saveBtn[0].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("9am", textarea[0].value);
+  })
 
+  saveBtn[1].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("10am", textarea[1].value);
+  })
+
+  saveBtn[2].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("11am", textarea[2].value);
+  })
+
+  saveBtn[3].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("12pm", textarea[3].value);
+  })
+
+  saveBtn[4].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("1pm", textarea[4].value);
+  })
+
+  saveBtn[5].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("2pm", textarea[5].value);
+  })
+
+  saveBtn[6].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("3pm", textarea[6].value);
+  })
+
+  saveBtn[7].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("4pm", textarea[7].value);
+  })
+
+  saveBtn[8].addEventListener("click", function(e) {
+    e.preventDefault();
+    localStorage.setItem("5pm", textarea[8].value);
+  })
 
 
   // Differentiate past, present, and future
+  
   var currentTime = dayjs().format('H');
 
   if (currentTime < 9) {
@@ -93,7 +137,9 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  
+  textarea[0].value = localStorage.getItem["9am"];
+  textarea[1].value = localStorage.getItem["10am"];
+
 
 
 
