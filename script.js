46042fad9bce881cda697b5ef2b4f8ed3aa4037d
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2022-12-05 23:07:59 -0800

    added current date in JS

--- script.js
+++ script.js
@@ -14,10 +14,21 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  // var currentTime = dayjs().format('hA');
+  // var timeDiv = document.querySelector(".hour");
+  // var timeBlock = timeDiv.textContent;
+  // var block = document.querySelector("row");
+  // console.log(timeBlock);
+  
+  // if (timeBlock = currentTime) {
+  //   block.classList.add("present");
+  // }
+  // //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+  var todayDate = dayjs().format('dddd, MMMM D');
+  $('#currentDay').text(todayDate);
+  });
