8e40eb0ef7ef34697e6917cd696106c6d6508254
Betty Lu <bettylu@Bettys-MacBook-Pro.local>
2022-12-06 00:11:13 -0800

    added code to differentiate time blocks

--- script.js
+++ script.js
@@ -8,27 +8,97 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  // var currentTime = dayjs().format('hA');
-  // var timeDiv = document.querySelector(".hour");
-  // var timeBlock = timeDiv.textContent;
-  // var block = document.querySelector("row");
-  // console.log(timeBlock);
   
-  // if (timeBlock = currentTime) {
-  //   block.classList.add("present");
-  // }
-  // //
+
+
+
+
+
+  // Differentiate past, present, and future
+  var currentTime = dayjs().format('H');
+
+  if (currentTime < 9) {
+    document.getElementById("hour-9").classList.add("future");
+  } else if (currentTime === 9 ) {
+    document.getElementById("hour-9").classList.add("present");
+  } else {
+    document.getElementById("hour-9").classList.add("past");
+  }
+  
+  if (currentTime < 10) {
+    document.getElementById("hour-10").classList.add("future");
+  } else if (currentTime === 10 ) {
+    document.getElementById("hour-10").classList.add("present");
+  } else {
+    document.getElementById("hour-10").classList.add("past");
+  }
+
+  if (currentTime < 11) {
+    document.getElementById("hour-11").classList.add("future");
+  } else if (currentTime === 11 ) {
+    document.getElementById("hour-11").classList.add("present");
+  } else {
+    document.getElementById("hour-11").classList.add("past");
+  }
+
+  if (currentTime < 12) {
+    document.getElementById("hour-12").classList.add("future");
+  } else if (currentTime === 12 ) {
+    document.getElementById("hour-12").classList.add("present");
+  } else {
+    document.getElementById("hour-12").classList.add("past");
+  }
+
+  if (currentTime < 13) {
+    document.getElementById("hour-1").classList.add("future");
+  } else if (currentTime === 13 ) {
+    document.getElementById("hour-1").classList.add("present");
+  } else {
+    document.getElementById("hour-1").classList.add("past");
+  }
+
+  if (currentTime < 14) {
+    document.getElementById("hour-2").classList.add("future");
+  } else if (currentTime === 14 ) {
+    document.getElementById("hour-2").classList.add("present");
+  } else {
+    document.getElementById("hour-2").classList.add("past");
+  }
+
+  if (currentTime < 15) {
+    document.getElementById("hour-3").classList.add("future");
+  } else if (currentTime === 15 ) {
+    document.getElementById("hour-3").classList.add("present");
+  } else {
+    document.getElementById("hour-3").classList.add("past");
+  }
+
+  if (currentTime < 16) {
+    document.getElementById("hour-4").classList.add("future");
+  } else if (currentTime === 16 ) {
+    document.getElementById("hour-4").classList.add("present");
+  } else {
+    document.getElementById("hour-4").classList.add("past");
+  }
+
+  if (currentTime < 17) {
+    document.getElementById("hour-5").classList.add("future");
+  } else if (currentTime === 17 ) {
+    document.getElementById("hour-5").classList.add("present");
+  } else {
+    document.getElementById("hour-5").classList.add("past");
+  }
+  
+  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  
+
+
+
+
+  // Display the current date in the header of the page.
   var todayDate = dayjs().format('dddd, MMMM D');
   $('#currentDay').text(todayDate);
   });
